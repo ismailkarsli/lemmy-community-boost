ea74dd220fecce52f00f6ffaf2d330538e2a12a8
İsmail Karslı <cszn@pm.me>
2023-12-13 03:09:32 +0300

    Unsubscribe script

--- reset.ts
+++ reset.ts
@@ -1,28 +1,48 @@
 // Unsubscribe from all subscriptions
+// For some reason, it needs to be run multiple times to unsubscribe from all communities
 
-import { communityDb, instanceDb } from "./database";
-import { followCommunity, getClient, getCommunity } from "./lib";
+import { instanceDb } from "./database";
+import { followCommunity, getClient } from "./lib";
 
-(async () => {
-  const instances = await instanceDb.findAsync({ active: true });
-  const communities = await communityDb.findAsync({}).sort({ updatedAt: 1 });
-
-  for (const instance of instances) {
-    for (const community of communities) {
-      try {
-        const client = await getClient(instance);
-        const remoteCommunity = await getCommunity(
-          client,
-          `${community.name}@${community.host}`
-        );
-
-        await followCommunity(client, {
-          community_id: remoteCommunity.community.id,
-          follow: false,
+const reset = async () => {
+  const instances = await instanceDb.findAsync({});
+  const promises = instances.map(async (instance) => {
+    try {
+      const client = await getClient(instance);
+      let page = 0;
+      while (true) {
+        const subscriptions = await client.listCommunities({
+          type_: "Subscribed",
+          limit: 50,
+          page: ++page,
+          auth: instance.jwt,
         });
-      } catch (e) {
-        console.error(e);
+        if (subscriptions.communities.length === 0) {
+          break;
+        }
+
+        for (const subscription of subscriptions.communities) {
+          await followCommunity(client, {
+            community_id: subscription.community.id,
+            follow: false,
+            // @ts-ignore
+            auth: instance.jwt,
+          });
+
+          console.log(
+            `${instance.host}: unsubscribed from ${subscription.community.actor_id}`
+          );
+        }
       }
+    } catch (e) {
+      console.error("error", e);
     }
-  }
-})();
+  });
+
+  await Promise.all(promises);
+};
+
+reset().then(() => {
+  console.log("Done");
+  process.exit(0);
+});
