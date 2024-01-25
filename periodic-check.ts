8c88c42a6eb0a72208ccaafa94bad1ba697eb4d1
İsmail Karslı <ismail@karsli.net>
2024-01-25 18:13:00 +0300

    Add initial delay before starting periodic check

--- periodic-check.ts
+++ periodic-check.ts
@@ -4,6 +4,8 @@ export const INTERVAL = Number(process.env.INTERVAL) || 1000 * 60 * 60; // 1 hou
 
 export const startPeriodicCheck = async () => {
   try {
+    // initial 60 seconds delay
+    await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
     while (true) {
       console.info("Periodic check started", new Date());
       await conditionalFollow();
